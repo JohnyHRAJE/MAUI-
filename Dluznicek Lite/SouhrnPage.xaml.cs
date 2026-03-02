88b3fdc3120c44325e20762054954ac5dda612a1
1kostir1@gmail.com <1kostir1@gmail.com>
2026-03-02 02:54:52 +0100

    uprava popisků

--- Dluznicek Lite/SouhrnPage.xaml.cs
+++ Dluznicek Lite/SouhrnPage.xaml.cs
@@ -7,7 +7,7 @@ public partial class SouhrnPage : ContentPage
     // Sem uložíme hotové výpočty pro zobrazení
     public ObservableCollection<PolozkaUctenky> Uctenky { get; set; } = new ObservableCollection<PolozkaUctenky>();
 
-    private double zbyvaVybrat = 0;
+    
     
     public SouhrnPage(ObservableCollection<Kamarad> pijaci, double cena10, double cena11, double cena12, double dyskoProcenta)
     {
